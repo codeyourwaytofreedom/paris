9966894a45c8a874cc31199e2e4db2414513a687
codeyourwaytofreedom <can.kolej@gmail.com>
2023-05-11 14:30:26 +0300

    vercel test

--- pages/api/coins.ts
+++ pages/api/coins.ts
@@ -6,6 +6,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     res.setHeader('Content-Type', 'text/event-stream');
     res.setHeader('Cache-Control', 'no-cache');
     res.setHeader('Connection', 'keep-alive');
+    res.setHeader('Content-Encoding', 'none');
+
 
     const sendCoinData = async () => {
       // Make the API request
